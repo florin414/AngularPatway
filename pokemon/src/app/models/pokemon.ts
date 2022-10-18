14d9b6466426f74edd1ba08a40d19b34a6d1f6dd
Florin Magadan <florin.magadan@nagarro.com>
2022-10-18 14:15:09 +0300

    feature: create models for: - pokemon data - user - product data

---
+++ pokemon/src/app/models/pokemon.ts
@@ -0,0 +1,5 @@
+export interface IPokemon{
+  imageUrl: string;
+  id: number;
+  pokemonName: string;
+}
---
+++ pokemon/src/app/models/user.ts
@@ -0,0 +1,8 @@
+
+export interface IUser{
+  id: number;
+  userame: string;
+  email: string;
+  password: string;
+  role: UserRole;
+}
