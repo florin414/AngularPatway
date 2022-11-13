f05503f9ca7aca3fc64a9dc110d603cac29a85de
Florin Magadan <florin.magadan@nagarro.com>
2022-11-13 20:24:48 +0200

    feature: - porgress bar component - porgress bar color directive

---
+++ pokemon/src/app/shared/progress-bar-color.directive.ts
@@ -0,0 +1,37 @@
+import {
+  Directive,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ElementRef,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appProgressBarColor]',
+})
+export class ProgressBarColorDirective implements OnChanges {
+  static counter = 0;
+
+  @Input() appProgressBarColor: any;
+  styleEl: HTMLStyleElement = document.createElement('style');
+
+  uniqueAttr = `app-progress-bar-color-${ProgressBarColorDirective.counter++}`;
+
+  constructor(private el: ElementRef) {
+    const nativeEl: HTMLElement = this.el.nativeElement;
+    nativeEl.setAttribute(this.uniqueAttr, '');
+    nativeEl.appendChild(this.styleEl);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateColor();
+  }
+
+  updateColor(): void {
+    this.styleEl.innerText = `
+      [${this.uniqueAttr}] .mat-progress-bar-fill::after {
+        background-color: ${this.appProgressBarColor};
+      }
+    `;
+  }
+}
